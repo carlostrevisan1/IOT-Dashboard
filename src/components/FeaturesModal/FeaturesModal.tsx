0c623d42b51fc9ed1eb61e183c4bcc2fa33a3451
Carlos Trevisan <xkoeplo@gmail.com>
2021-11-05 19:02:52 -0300

    reafctor

--- src/components/FeaturesModal/FeaturesModal.tsx
+++ src/components/FeaturesModal/FeaturesModal.tsx
@@ -1,4 +1,4 @@
-import { Button,Card,List } from 'antd';
+import { Button, Card, List } from 'antd';
 import Modal from 'antd/lib/modal/Modal';
 import React, { useState } from 'react';
 import { FeaturesSchema } from '../../constants/device';
@@ -25,25 +25,25 @@ type Props = {
 }
 
 export default function FeaturesModal(
-  { visible, 
+  { visible,
     handleClose,
     handleSave,
-    features 
-  } : Props){
-      
+    features
+  }: Props) {
+
   const [visibleForm, setVisibleForm] = useState(true);
   const [visibleList, setVisibleList] = useState(false);
-  
-  function handleFinish(val: any){
+
+  function handleFinish(val: any) {
     handleSave(val);
     handleClose();
   }
 
   return (
     <div>
-      <Modal 
-        title="Edit Features" 
-        visible={visible} 
+      <Modal
+        title="Edit Features"
+        visible={visible}
         onCancel={handleClose}
         destroyOnClose
         footer={[
@@ -52,48 +52,49 @@ export default function FeaturesModal(
           </Button>,
 
           <Button form="NewDeviceForm" type="primary" htmlType="submit" >
-                Add
+            Add
           </Button>,
         ]} >
-          {/*! LIST */}
-          <div hidden={visibleList}>
+          
+        {/*! LIST */}
+        <div hidden={visibleList}>
 
-            <List
-              itemLayout='vertical'
-              dataSource={features}
-              renderItem={item =>(
-                <List.Item>
-                  <Card 
+          <List
+            itemLayout='vertical'
+            dataSource={features}
+            renderItem={item => (
+              <List.Item>
+                <Card
                   hoverable
                   onClick={() => {
                     // TODO SET Features data to modal and clear list
                     setVisibleForm(!visibleForm)
                     setVisibleList(true);
-                    }}>
+                  }}>
 
-                    <Card.Grid style={{
-                      width: '100%'
-                      }}
-                    >
-                      <div style={{ display: 'flex', textAlign: 'center', justifyContent: 'space-between'}}>
-                        <span style={{display: 'inline-block', verticalAlign: 'middle', lineHeight: 'normal'}}>
-                          {item.name }
-                        </span>
-                        <EditOutlined style={{}}/>
-                      </div>
+                  <Card.Grid style={{
+                    width: '100%'
+                  }}
+                  >
+                    <div style={{ display: 'flex', textAlign: 'center', justifyContent: 'space-between' }}>
+                      <span style={{ display: 'inline-block', verticalAlign: 'middle', lineHeight: 'normal' }}>
+                        {item.name}
+                      </span>
+                      <EditOutlined style={{}} />
+                    </div>
 
-                    </Card.Grid>
-                  </Card>
-                </List.Item>
-              )
+                  </Card.Grid>
+                </Card>
+              </List.Item>
+            )
 
-              }
-            />
-          </div>
+            }
+          />
+        </div>
 
-          { /* Form */}
-          <FeaturesEditForm id="NewDeviceForm" onFinish={handleFinish} visible={visibleForm}/>
-          
+        { /* Form */}
+        <FeaturesEditForm id="NewDeviceForm" onFinish={handleFinish} visible={visibleForm} />
+
       </Modal>
     </div>
   );
--- src/components/StandardCard/StandardCard.tsx
+++ src/components/StandardCard/StandardCard.tsx
@@ -15,72 +15,72 @@ type Props = {
   colour: string;
 }
 
-export default function StandardCard({ deviceTitle, features, colour }: Props){
+export default function StandardCard({ deviceTitle, features, colour }: Props) {
 
   const [showEditModal, setShowEditModal] = useState(false)
   const [spinSettings, setSpin] = useState(false);
 
 
-  function handleEditModal(){
+  function handleEditModal() {
     setShowEditModal(!showEditModal);
   }
 
-  function handleCreateFeatures(){
-    const toAddFeats = features?.map( feat => {
-      switch(feat.type){
+  function handleCreateFeatures() {
+    const toAddFeats = features?.map(feat => {
+      switch (feat.type) {
         case 1:
           return <Button>{feat.name}</Button>
           break;
         case 2:
-          return <Switch/>
+          return <Switch />
           break;
-        case 3: 
-          return <Slider/>
+        case 3:
+          return <Slider />
           break;
         case 4:
-          return <StandardInput label={feat.name}/>
-        default: 
+          return <StandardInput label={feat.name} />
+        default:
           return;
-          
+
       }
     })
     return toAddFeats;
   }
 
   return (
-    <div style={{ 
-      marginLeft: 5, 
+    <div style={{
+      marginLeft: 5,
       flexGrow: 1,
       maxWidth: 350,
       minWidth: 350,
       flexShrink: 0,
       marginTop: 10,
-      }}>
+    }}>
 
-      <Card style={{ 
-        borderRadius: 10, 
+      <Card style={{
+        borderRadius: 10,
         background: `linear-gradient(182deg, ${colour} 65px, #1f1f1f 66px)`,
-        
-        }} >
-        
-        <Header  style={{
-          flexDirection: 'row', 
-          display: 'flex', 
+
+      }} >
+
+        <Header style={{
+          flexDirection: 'row',
+          display: 'flex',
           padding: 0,
           backgroundColor: "transparent",
           marginTop: -27,
           marginBottom: 20,
-          }}>
+        }}>
 
-            <Typography style={{
-              textOverflow: "ellipsis",
-              overflow: "hidden",
-              whiteSpace: "nowrap",
-              fontWeight: 600,
-              fontSize: "1.2rem",
-              width: "calc(100% - 30px)",
-              
-            }}>{deviceTitle}</Typography>
+          <Typography style={{
+            textOverflow: "ellipsis",
+            overflow: "hidden",
+            whiteSpace: "nowrap",
+            fontWeight: 600,
+            fontSize: "1.2rem",
+            width: "calc(100% - 30px)",
+
+          }}>{deviceTitle}</Typography>
 
           <span style={{
             // marginTop: "25px",
@@ -88,27 +88,27 @@ export default function StandardCard({ deviceTitle, features, colour }: Props){
             fontSize: "20px",
             cursor: "pointer",
           }}>
-            <SettingOutlined 
+            <SettingOutlined
               onClick={handleEditModal}
               spin={spinSettings}
               onMouseEnter={() => setSpin(!spinSettings)}
               onMouseLeave={() => setSpin(!spinSettings)}
-            /> 
+            />
           </span>
-          
+
 
         </Header>
-          
-        <div style={{marginTop: 10}}>
+
+        <div style={{ marginTop: 10 }}>
           {handleCreateFeatures()}
         </div>
       </Card>
-      <FeaturesModal 
-        handleClose={handleEditModal} 
-        visible={showEditModal} 
-        handleSave={(va) => {console.log(va)}}
+      <FeaturesModal
+        handleClose={handleEditModal}
+        visible={showEditModal}
+        handleSave={(va) => { console.log(va) }}
         features={features}
-        />
+      />
     </div>
   );
 };
